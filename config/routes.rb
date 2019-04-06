d2da720eefd148559cb7b57bed9aa5e0b6c81726
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-04-06 10:48:28 -0700

    Build User Model via gem bcrypt, Users & Sessions Controllers with routes & views and application controller for user authentication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,11 @@
+# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
 Rails.application.routes.draw do
+  
   get("/", to: "pages#home", as: :root)
-  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  resources :users, only: [ :new, :create]
+  
+  resource :session, only: [ :new, :create, :destroy]
+
 end
