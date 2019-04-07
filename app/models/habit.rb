3568b447d8a7bea2e00d9f112a74c33f91d84e7a
Amir Helmy <amir.helmy@mail.mcgill.ca>
2019-04-06 17:10:23 -0700

    Refactor Habit & TackledHabit models to default some attributes before validation

--- app/models/habit.rb
+++ app/models/habit.rb
@@ -3,12 +3,25 @@ class Habit < ApplicationRecord
   belongs_to :user
   has_many :tackled_habits, dependent: :destroy
 
+  before_validation(:set_default_frequency)
+  before_validation(:set_default_number_of_days)
+
   validates :name, presence: true, uniqueness: true
   validates :habit_type, presence: true, inclusion: {in: ["binary","number"]}
   validates :threshold, numericality: { greater_than_or_equal_to: 0 }
   validates :min_or_max, inclusion: {in: %w(least most)}
   validates :target_streak, presence: true, numericality: { only_integer: true, greater_than: 0 }
-  # validates :frequency, inclusion: {in: %w(daily weekly monthly yearly)}
-  # validates :number_of_days, numericality: { only_integer: true, greater_than: 0 }
+  validates :frequency, inclusion: {in: %w(daily weekly monthly yearly)}
+  validates :number_of_days, numericality: { only_integer: true, greater_than: 0 }
+
+  private 
+
+  def set_default_frequency
+    self.frequency ||= 'daily'
+  end
+
+  def set_default_number_of_days
+    self.number_of_days ||= 1
+  end
 
 end
--- app/models/tackled_habit.rb
+++ app/models/tackled_habit.rb
@@ -3,16 +3,15 @@ class TackledHabit < ApplicationRecord
   belongs_to :user
   belongs_to :habit
 
-  validates :current_streak, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
-  validates :maximum_streak, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
-  validates :number_of_attempts, numericality: { only_integer: true, greater_than_or_equal_to: 0 }, allow_nil: true
-  validates :success_percent, numericality: { greater_than_or_equal_to: 0 }, allow_nil: true
-
   before_validation(:set_default_current_streak)
   before_validation(:set_default_maximum_streak)
   before_validation(:set_default_number_of_attempts)
   before_validation(:set_default_success_percent)
 
+  validates :current_streak, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :maximum_streak, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :number_of_attempts, numericality: { only_integer: true, greater_than_or_equal_to: 0 }
+  validates :success_percent, numericality: { greater_than_or_equal_to: 0 }
 
   private
 
